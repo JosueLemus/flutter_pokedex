b5f5a4da398e058f0918d093e0ff044ef91dda0d
JosueLemus <josueunueve@gmail.com>
2024-02-24 19:36:22 -0400

    Add list of types

---
+++ lib/config/constants/list_of_types.dart
@@ -0,0 +1,22 @@
+class FiltersContants {
+  static List<String> pokemonTypes = [
+    'dark',
+    'dragon',
+    'electric',
+    'fairy',
+    'fighting',
+    'fire',
+    'flying',
+    'ghost',
+    'grass',
+    'ground',
+    'ice',
+    'normal',
+    'poison',
+    'psychic',
+    'rock',
+    'steel',
+    'bug',
+    'water'
+  ];
+}
