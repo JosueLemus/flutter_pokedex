2502a02910a7d10248c26fef9b21912f37f5d222
JosueLemus <josueunueve@gmail.com>
2024-02-24 11:57:58 -0400

    Add filters

--- lib/presentation/widgets/home_appbar.dart
+++ lib/presentation/widgets/home_appbar.dart
@@ -7,11 +7,52 @@ class HomeAppBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Image.asset(
-          "assets/images/gradient-pokeball.png",
-          color: const Color(0xFFB5B9C4).withOpacity(0.05),
-        ),
+        Image.asset("assets/images/gradient-pokeball.png",
+            // color: const Color(0xFFB5B9C4).withOpacity(0.05),
+            color: Colors.red),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(23, 50, 23, 23),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  _FilterButton(
+                      asset: 'assets/images/home-filters/Generation.png',
+                      onTap: () {}),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: _FilterButton(
+                        asset: 'assets/images/home-filters/Sort.png',
+                        onTap: () {}),
+                  ),
+                  _FilterButton(
+                      asset: 'assets/images/home-filters/Filter.png',
+                      onTap: () {}),
+                ],
+              ),
+            ],
+          ),
+        )
       ],
+    );
+  }
+}
+
+class _FilterButton extends StatelessWidget {
+  final String asset;
+  final Function() onTap;
+  const _FilterButton({super.key, required this.asset, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Image.asset(
+        asset,
+        width: 25,
+        height: 25,
+      ),
     );
   }
 }
